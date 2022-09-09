ded7999a6700c3807bfa1de2dbc337463c26b8fa
Yerardinego <yerardine.sua@unillanos.edu.com>
2022-09-08 22:32:27 -0500

    Ejemplos de Funacione con Dart

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() 
+{
+  print(obtenerNombre("Yeral"));
+  print(obtenerNombreApellido("Yeral","Sua"));
+  print(obtenerNombreApellidoNameParameter(apellido: "Sua"));
+}
+String obtenerNombre(String valor){
+  return 'Mi nombre es $valor';
+  }
+String obtenerNombreApellido(String nombre, String apellido){
+  return 'Mi nombre es $nombre $apellido';
+  }
+String obtenerNombreApellidoNameParameter(
+  {String? nombre, required String apellido, int edad=28}){
+  return 'Mi nombre es $nombre $apellido $edad';
+  }
+  //function anonymus
+  var myFuncionAnonima =(String valor){
+    return "el valor es $valor";
+  };
+
