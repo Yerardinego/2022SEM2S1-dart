717392b1233a9e05bfefbd60db8528700babf61f
Yerardinego <yerardine.sua@unillanos.edu.com>
2022-09-08 21:57:28 -0500

    Ejemplos de variables con Dart

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+  
+  var nombre1 = "Yeral";
+  String nombre2 = "Yeral";
+
+  final String nombre3 = 'Thomas';
+  
+  const String nombre4 = 'Anyi';
+ 
+  
+  print(nombre1);
+  print(nombre2);
+}
